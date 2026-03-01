84231a14ea6ef2906dcc61799deb825c1a8c1421
revathiganeshinfo-cpu <revathiganeshinfo@gmail.com>
2026-03-01 23:41:57 +0530

    Fixed MongoDB env variable issue and DB connection

--- Database/config.js
+++ Database/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log("MongoDB Connection Error:", error);
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import userRoutes from "./Routers/user.router.js";
+import connectDB from "./Database/config.js";
+
 
 dotenv.config();
 const app = express();
@@ -16,15 +18,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", userRoutes);
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(" Connected to MongoDB");
-  } catch (error) {
-    console.log(" MongoDB Connection Error:", error);
-    process.exit(1);
-  }
-};
+
 
 connectDB();
 
